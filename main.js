15da9af2ecb7d9c75a1e531991728c7103ec9780
Shawn Gates <gates.shawn@gmail.com>
2019-08-09 20:14:14 -0700

    Finished cleaning up the dynamic loader

--- main.js
+++ main.js
@@ -1,17 +1,45 @@
+/*	
+//	Copyright 2019 Shawn Gates
+//
+//	Licensed under the Apache License, Version 2.0 (the "License");
+//	you may not use this file except in compliance with the License.
+//	You may obtain a copy of the License at
+//
+//  http://www.apache.org/licenses/LICENSE-2.0
+//
+//	Unless required by applicable law or agreed to in writing, software
+// 	distributed under the License is distributed on an "AS IS" BASIS,
+//	WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// 	See the License for the specific language governing permissions and
+// 	limitations under the License.
+*/
+
+// setup libs
 const Discord = require('discord.js');
 const fs = require('fs');
 const secret_token = require('./secret_token.js');
 
+// global constants
 const client = new Discord.Client();
 
+// global variables
 let message = {};
 let callbacks = {};
 
+/**
+ * Adds items to the psuedoSwitch functions callback list
+ * @param {String}   _case Value to use for the 'switch case'
+ * @param {Function} fn    Function to call when _case match is made
+ */
 function add(_case, fn) {
    callbacks[_case] = callbacks[_case] || [];
    callbacks[_case].push(fn);
 }
 
+/**
+ * Function that works like a switch case but allows for 'cases' to be added dynamiclly
+ * @param  {String} value String to check against the callback list
+ */
 function pseudoSwitch(value) {
    if (callbacks[value]) {
       callbacks[value].forEach(function(fn) {
@@ -20,6 +48,7 @@ function pseudoSwitch(value) {
    }
 }
 
+// check the module folder for modules and add them to the switch for loading
 fs.readdir('./modules', function callback(err, files) {
 	if(err) console.log (err);
 	console.log('Loading System Modules: ' + files);
@@ -31,6 +60,8 @@ fs.readdir('./modules', function callback(err, files) {
 	}
 });
 
+
+// list connected servers and channels when we connect to the Discord service
 client.on('ready', () => {
     console.log("Connected as " + client.user.tag);
     // List servers the bot is connected to
@@ -43,9 +74,10 @@ client.on('ready', () => {
             console.log(` -- ${channel.name} (${channel.type}) - ${channel.id}`);
         });
     });
-
 });	
 
+
+// listen for messages from the bot
 client.on('message', (receivedMessage) => {
     // Prevent bot from responding to its own messages
     if (receivedMessage.author == client.user) {
@@ -64,5 +96,7 @@ client.on('message', (receivedMessage) => {
 
 });
 
+
+// log in to the bot with the secret token
 let bot_secret_token = secret_token.bot_secret_token;
 client.login(bot_secret_token);
