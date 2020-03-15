dc0fa46e3caa3a7cbc025b2b04010b9e97737caa
Shawn Gates <gates.shawn@gmail.com>
2020-03-15 11:37:29 -0700

    Rote support added fully

--- modules/cod.js
+++ modules/cod.js
@@ -1,5 +1,5 @@
 /*	
-//	Copyright 2019 Shawn Gates
+//	Copyright 2020 Shawn Gates
 //
 //	Licensed under the Apache License, Version 2.0 (the "License");
 //	you may not use this file except in compliance with the License.
@@ -14,20 +14,24 @@
 // 	limitations under the License.
 */
 
-const die = require('../utils/die.js');
+const Die = require('../utils/die.js');
 
 let cod_die = 10;
 let cod_tn = 8;
+let cod_ES = 5;
+
 let results = [];
 let results_explosion = [];
 let success = [];
-let success_explosion = [];
+let results_reroll = [];
 
 /**
  * Calls die rolling for Chronicles of Darkness
  * @param  Object options Parameter object
  * @param Boolean options.chance_die Sets whether the roll being made is a chance die
- * @param Number options.again value to use for rolling again
+ * @param Number options.again Value to use for rolling again
+ * @param Number options.explosion Flag if the current roll is a re-roll from an exploding die
+ * @param Number options.rote Flag thats shows whether a roll shoudld be rote
  * @return {[type]}         [description]
  */
 function roll(options) {
@@ -36,7 +40,7 @@ function roll(options) {
 		return;
 	}
 
-	let result = die.die.roll(cod_die);
+	let result = Die.die.roll(cod_die);
 	console.log('Roll Result: ' + result);
 	if(!options.explosion) {
 		results.push(result);
@@ -45,145 +49,166 @@ function roll(options) {
 	}
 	if(result >= cod_tn) {success.push(result);}
 
-	console.log(results);
-	console.log(results_explosion);
-	// console.log(success);
+	if(result < cod_tn && options.rote && !options.chance_die) {
+		console.log('Rerolling failures once with Rote.');
+		roll({
+			again: options.again,
+		});	
+	}
 
 	if(result >= options.again && !options.chance_die) {
 		console.log('Roll was ' + result + ', which meets or beats the "again" threshold of ' + options.again);
 		roll({
-			explosion: true
+			explosion: true,
+			again: options.again,
+			rote: options.rote
 		});		
 	}
 
 }
 
+/**
+ * [responseBuilder description]
+ * @param  {Object} receivedMessage [description]
+ * @return {[type]}                 [description]
+ */
 function responseBuilder(receivedMessage) {
+	let rote = false;
 	let chance_die = false;
-		let again = 10;
-		let re_die = /[0-9]+/;
-		let re_again = /8|9-again|no-again/;
-		let die_match = receivedMessage.content.match(re_die);
-		if (die_match===null) {die_match = [0];}
-		console.log('Number of dice to be rolled: ' + die_match);
-		let again_match = receivedMessage.content.match(re_again);
-		console.log(again_match);
-		switch (again_match[0]) {
-			case '9-again':
-				again = 9;
-				break;
-			case '8-again':
-				again = 8;
-				break;
-			case 'no-again':
-				again = 11;
-				break;
-			default:
-		}
-		console.log('Again type: ' + again);
+	let again = 10;
+	let re_die = /[0-9]+\s/;
+	let re_again = /8-again|9-again|no-again/;
+	let re_rote = /rote/;
 
-		// Reject die pools over 100, large die pools cause server slow down and 100 is plenty of buffer space
-		if (die_match > 100) {
-			receivedMessage.channel.send(receivedMessage.author.toString() + ', your roll was rejected because it was too large. Please roll again with a smaller dice pool.');
-			return;
-		}
-		let die_count = die_match[0];
+	let die_match = receivedMessage.content.match(re_die);
+	if (die_match === null) {
+		receivedMessage.channel.send(receivedMessage.author.toString() + ", you didn't specify a number of dice to roll. Please try again.");
+		return;
+	}
+	console.log('Number of dice to be rolled: ' + die_match);
+	let again_match = receivedMessage.content.match(re_again);
+	if (again_match === null) {again_match = '';}
+	console.log(again_match);
+	let rote_match = receivedMessage.content.match(re_rote);
+	if (rote_match === null) {rote_match = '';}
+	console.log(rote_match);
+	rote = rote_match[0] ? true : false;
+	console.log(rote);
+	
+	switch (again_match[0]) {
+		case '9-again':
+			again = 9;
+			break;
+		case '8-again':
+			again = 8;
+			break;
+		case 'no-again':
+			again = 11;
+			break;
+		default:
+	}
+	console.log('Again type: ' + again);
 
-		if(die_count == 0) {
-			die_count = 1;
-			chance_die = true;
-		}
+	// Reject die pools over 100, large die pools cause server slow down and 100 is plenty of buffer space
+	if (die_match > 100) {
+		receivedMessage.channel.send(receivedMessage.author.toString() + ', your roll was rejected because it was too large. Please roll again with a smaller dice pool.');
+		return;
+	}
+	let die_count = die_match[0];
 
-		for (var i = die_count-1; i >= 0; i--) {
-			roll({
-				chance_die: chance_die,
-				again: again,
-			});
-		}
+	if(die_count == 0) {
+		die_count = 1;
+		chance_die = true;
+	}
 
-		let response = receivedMessage.author.toString() + ', you rolled: '
+	for (var i = die_count-1; i >= 0; i--) {
+		roll({
+			chance_die: chance_die,
+			again: again,
+			rote:rote,
+		});
+	}
 
-		for (let i = results.length-1; i >= 0; i--) {
-				let result_print = ''
-				if (results[i] >= 8) {
-					if (chance_die) {
-						if (results[i] != 10) {
-							result_print = results[i];
-						} else {
-							result_print = '**' + results[i] + '**';
-						}
-					} else {
-						result_print = '**' + results[i] + '**';
-					}
-				} else {
+	let response = receivedMessage.author.toString() + ', you rolled: '
+
+	for (let i = results.length-1; i >= 0; i--) {
+		let result_print = '';
+		if (results[i] >= cod_tn) {
+			if (chance_die) {
+				if (results[i] != 10) {
 					result_print = results[i];
-				}
-				//add a comma between results
-				if(i > 0) { 
-					response += result_print + ', ';
-				} else if(i === 0 && results_explosion.length != 0) {
-					response += result_print + ', ';
 				} else {
-					response += result_print;
+					result_print = '**' + results[i] + '**';
 				}
-		}
-		for (let i = results_explosion.length-1; i >= 0; i--) {
-			let result_print = ''
-				if (results_explosion[i] >= 8) {
-					if (chance_die) {
-						if (results_explosion[i] != 10) {
-							result_print = results_explosion[i];
-						} else {
-							result_print = '**' + results_explosion[i] + '**';
-						}
-					} else {
-						result_print = '**' + results_explosion[i] + '**';
-					}
-				} else {
-					result_print = results_explosion[i];
-				}
-				//add a comma between results
-				if(i > 0) { 
-					response += result_print + ', ';
-				} else {
-					response += result_print;
-				}
-		}
-		console.log('Success response to server: ' + response);
-		receivedMessage.channel.send(response);
-
-		let success_response = '';
-
-		if (chance_die) {
-			if(success[0] == 10) {
-				success_response = "Success! You rolled a 10 on a chance die!";
-			} else if(success[0] == 1) {
-				success_response = "You got a Dramatic Failure!";
 			} else {
-				success_response = "You didn't roll a success. That's a Failure. Would you like to make it a Dramatic Failure for a Beat?";
+				result_print = '**' + results[i] + '**';
 			}
-
 		} else {
-
-			// success counting
-			if(success.length == 0) {
-				// failure
-				success_response = "You rolled no successes. That's a Failure. Would you like to make it a Dramatic Failure for a Beat?";
-			} else if (success.length >= 5) {
-				//exceptional success
-				success_response = "You rolled " + success.length + " successes. That's an Exceptional Success!";
-			} else {
-				//regular success
-				success_response = "You rolled " + success.length + " successes.";
-			}
-
+			result_print = results[i];
 		}
+		//add a comma between results
+		if(i > 0) { 
+			response += result_print + ', ';
+		} else if(i === 0 && results_explosion.length != 0) {
+			response += result_print + ', ';
+		} else {
+			response += result_print;
+		}
+	}
+	for (let i = results_explosion.length-1; i >= 0; i--) {
+		let result_print = '';
+		if (results_explosion[i] >= cod_tn) {
+			if (chance_die) {
+				if (results_explosion[i] != 10) {
+					result_print = results_explosion[i];
+				} else {
+					result_print = '**' + results_explosion[i] + '**';
+				}
+			} else {
+				result_print = '**' + results_explosion[i] + '**';
+			}
+		} else {
+			result_print = results_explosion[i];
+		}
+		//add a comma between results
+		if(i > 0) { 
+			response += result_print + ', ';
+		} else {
+			response += result_print;
+		}
+	}
+	console.log('Success response to server: ' + response);
+	receivedMessage.channel.send(response);
 
-		console.log('Results response to server: ' + success_response);
-		receivedMessage.channel.send(success_response);
+	let success_response = '';
 
-		results = [];
-		success = [];
+	if (chance_die) {
+		if(success[0] == 10) {
+			success_response = "Success! You rolled a 10 on a chance die!";
+		} else if(success[0] == 1) {
+			success_response = "You got a Dramatic Failure!";
+		} else {
+			success_response = "You didn't roll a success. That's a Failure. Would you like to make it a Dramatic Failure for a Beat?";
+		}
+	} else {
+		// success counting
+		if(success.length == 0) {
+			// failure
+			success_response = "You rolled no successes. That's a Failure. Would you like to make it a Dramatic Failure for a Beat?";
+		} else if (success.length >= cod_ES) {
+			//exceptional success
+			success_response = "You rolled " + success.length + " successes. That's an Exceptional Success!";
+		} else {
+			//regular success
+			success_response = "You rolled " + success.length + " successes.";
+		}
+	}
+	console.log('Results response to server: ' + success_response);
+	receivedMessage.channel.send(success_response);
+
+	results = [];
+	success = [];
+	results_explosion = [];
 }
 
 exports.call = '/cod';
