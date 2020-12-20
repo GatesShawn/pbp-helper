ecf55aac93ae4dc69df72f8568880b5db4ff11f4
Shawn Gates <gates.shawn@gmail.com>
2020-12-20 11:48:16 -0800

    Added Starting ability core generation

--- source/modules/AVOIAS/AVOIAS.js
+++ source/modules/AVOIAS/AVOIAS.js
@@ -68,6 +68,100 @@ class Character {
 }
 
 /**
+ * [rollAbilityScores description]
+ * @param {Object} race The race of the character
+ * @param {Object} race The subrace of the character
+ * @return {[type]} [description]
+ */
+function rollAbilityScores(race, subrace) {
+
+    let scores = {
+        "Strength": Die.die.roll(10) + 3,
+        "Dexterity": Die.die.roll(10) + 3,
+        "Constitution": Die.die.roll(10) + 3,
+        "Intelligence": Die.die.roll(10) + 3,
+        "Wisdom": Die.die.roll(10) + 3,
+        "Charisma": Die.die.roll(10) + 3
+    }
+
+    switch (race) {
+        case 'Deva':
+            if (scores.Wisdom < 10) {scores.Wisdom = 10};
+            if (scores.Intelligence < 8) {scores.Intelligence = 8};
+            if (scores.Charisma < 8) {scores.Charisma = 8};
+            break;
+        case 'Dragonborn':
+            if (scores.Strength < 10) {scores.Strength = 10};
+            if (scores.Constitution < 8) {scores.Constitution = 8};
+            if (scores.Charisma < 8) {scores.Charisma = 8};
+            break;
+        case 'Dwarf':
+            if (scores.Constitution < 12) {scores.Constitution = 12};
+            if (scores.Wisdom < 8) {scores.Wisdom = 8};
+            break;
+        case  'Elf':
+            if (scores.Dexterity < 10) {scores.Dexterity = 10};
+            if (scores.Intelligence < 10) {scores.Intelligence = 10};
+            break;
+        case 'Forged':
+            if (scores.Constitution < 10) {scores.Constitution = 10};
+            if (scores.Intelligence < 8) {scores.Intelligence = 8};
+            if (scores.Strength < 8) {scores.Strength = 8};
+            break;
+        case  'Genasi':
+            if (scores.Constitution < 8) {scores.Constitution = 8};
+            switch(subrace) {
+                case 'Air':
+                    if (scores.Dexterity < 12) {scores.Dexterity = 12};
+                    break;
+                case 'Earth':
+                    if (scores.Strength < 12) {scores.Strength = 12};
+                    break;
+                case 'Fire':
+                    if (scores.Charisma < 12) {scores.Charisma = 12};
+                    break;
+                case 'Water':
+                    if (scores.Wisdom < 12) {scores.Wisdom = 12};
+                    break;
+            }
+            break;
+        case 'Goliath':
+            if (scores.Strength < 12) {scores.Strength = 12};
+            if (scores.Constitution < 8) {scores.Constitution = 8};
+            break;
+        case 'Halfling':
+            if (scores.Dexterity < 12) {scores.Dexterity = 12};
+            if (scores.Charisma < 8) {scores.Charisma = 8};
+            break;
+        case 'Kith':
+            if (scores.Wisdom < 10) {scores.Wisdom = 10};
+            if (scores.Charisma < 10) {scores.Charisma = 10};
+            break;
+        case 'Shade':
+            if (scores.Intelligence < 10) {scores.Intelligence = 10};
+            if (scores.Wisdom < 8) {scores.Wisdom = 8};
+            if (scores.Intelligence < 8) {scores.Intelligence = 8};
+            break;
+        case  'Tabaxi':
+            if (scores.Strength < 8) {scores.Strength = 8};
+            if (scores.Dexterity < 8) {scores.Dexterity = 8};
+            if (scores.Constitution < 8) {scores.Constitution = 8};
+            if (scores.Wisdom < 8) {scores.Wisdom = 8};
+            break;
+        case  'Tiefling':
+            if (scores.Charisma < 10) {scores.Charisma = 10};
+            if (scores.Intelligence < 8) {scores.Intelligence = 8};
+            if (scores.Constitution < 8) {scores.Constitution = 8};
+            break;
+        default:
+            break;
+    }
+
+    return scores
+}
+
+
+/**
  * [responseBuilder description]
  * @param  {[type]} message [description]
  * @return {[type]}         [description]
@@ -90,14 +184,48 @@ function responseBuilder(message) {
         backgroundNum: Die.die.roll(20),
     }
 
+
+
     let char = new Character(rolls);
-    console.log(char);
+
+    let abilities = rollAbilityScores(char.race, char.subrace);
 
     let response = message.author + strings.intro;
+
+    // response is built in pieces to account for future modularity. i.e. not everyone will want random class
+
     // add race
     response += strings.race_1 + char.race + strings.race_2;
     // add sub race
     response += strings.subrace_1 + char.subrace + strings.subrace_2;
+
+    //
+    response += strings.startingAbilityScores_1;
+
+    for (let score in abilities) {
+        response += score + ': ' + abilities[score] + ', ';
+    }
+
+    response += strings.startingAbilityScores_2;
+
+    // add sub race
+    response += strings.familyLife_1 + char.familyLife + strings.familyLife_2;
+    // add sub race
+    response += strings.culture_1 + char.culture + strings.culture_2;
+    // add sub race
+    response += strings.status_1 + char.status + strings.status_2;
+    // add sub race
+    response += strings.religion_1 + char.religion + strings.religion_2;
+        // add sub race
+    response += strings.occupation_1 + char.occupation + strings.occupation_2;
+        // add sub race
+    response += strings.event_1 + char.event + strings.event_2;
+        // add sub race
+    response += strings.associate_1 + char.associate + strings.associate_2;
+        // add sub race
+    response += strings.class_1 + char.class + strings.class_2;
+        // add sub race
+    response += strings.background_1 + char.background + strings.background_2;
 
     message.channel.send('', new messageBuilder.message('AVOIAS', response))
         .catch(console.error);
